9b9f459755ad00bde8fc9a60a357c948ffc32e75
duckode <73285767+duckodes@users.noreply.github.com>
2025-03-18 15:55:17 +0800

    Add files via upload

---
+++ FileExtensionUnInstaller/FileExtensionUnInstaller/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace FileExtensionUnInstaller
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 移除註冊的副檔名名稱
+        /// </summary>
+        private const string ExtensionName = "filexins";
+        static void Main(string[] args)
+        {
+            if (!IsRunAsAdmin())
+            {
+                var procInfo = new ProcessStartInfo
+                {
+                    FileName = Process.GetCurrentProcess().MainModule.FileName,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+                try
+                {
+                    Process.Start(procInfo);
+                }
+                catch
+                {
+                    Console.WriteLine("無法以管理員權限運行！");
+                }
+                return;
+            }
+            Console.WriteLine("程序已以管理員權限運行！");
+
+            Unregistry();
+            Console.WriteLine("按任意鍵關閉此視窗…");
+            Console.ReadKey();
+        }
+        private static bool IsRunAsAdmin()
+        {
+            using var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        private static void Unregistry()
+        {
+            UnregistryIcon.UnregisterFileExtension(ExtensionName);
+        }
+    }
+}
---
+++ FileExtensionUnInstaller/FileExtensionUnInstaller/UnregistryIcon.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace FileExtensionUnInstaller
+{
+    public class UnregistryIcon
+    {
+        /// <summary>
+        /// 存放圖標的註冊資料夾名稱
+        /// </summary>
+        private const string DefaultIcon = "ExtensionIcon";
+        /// <summary>
+        /// 取消註冊副檔名圖標
+        /// </summary>
+        public static void UnregisterFileExtension(string extension)
+        {
+            try
+            {
+                // 刪除HKEY_CLASSES_ROOT\.yourfileextension
+                Registry.ClassesRoot.DeleteSubKeyTree("." + extension, false);
+
+                // 刪除關聯的默認圖標項(如果存在)
+                Registry.ClassesRoot.DeleteSubKeyTree(DefaultIcon, false);
+                Registry.ClassesRoot.DeleteSubKeyTree(extension, false);
+
+                // 更新文件關聯
+                NativeMethods.SHChangeNotify(NativeMethods.SHCNE_ASSOCCHANGED, NativeMethods.SHCNF_IDLIST, IntPtr.Zero, IntPtr.Zero);
+                Console.WriteLine($".{extension} 取消註冊。");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("發生錯誤：" + ex.Message);
+            }
+        }
+    }
+
+    internal class NativeMethods
+    {
+        public const uint SHCNE_ASSOCCHANGED = 0x08000000;
+        public const uint SHCNF_IDLIST = 0x0000;
+
+        [DllImport("Shell32.dll")]
+        public static extern void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+    }
+}
