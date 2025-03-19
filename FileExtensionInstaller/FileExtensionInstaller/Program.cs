4e0b2a8ec1da942004c4b251c1726623487c3b31
duckode <73285767+duckodes@users.noreply.github.com>
2025-03-19 10:33:48 +0800

    Add files via upload

---
+++ FileExtensionInstaller/FileExtensionInstaller/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace FileExtensionInstaller
+{
+    /// <summary>
+    /// 加入副檔名圖標
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 要註冊的副檔名名稱
+        /// </summary>
+        private const string ExtensionName = "filexins";
+        /// <summary>
+        /// 圖標檔案名稱
+        /// </summary>
+        private const string IconName = "filexins.ico";
+        static void Main(string[] args)
+        {
+            if (!IsRunAsAdmin())
+            {
+                var procInfo = new ProcessStartInfo
+                {
+                    FileName = Process.GetCurrentProcess().MainModule.FileName,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+                try
+                {
+                    Process.Start(procInfo);
+                }
+                catch
+                {
+                    Console.WriteLine("無法以管理員權限運行！");
+                }
+                return;
+            }
+            Console.WriteLine("程序已以管理員權限運行！");
+
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory + "Resource\\" + IconName;
+            CheckRegistryIcon(directoryPath);
+            Console.WriteLine("按任意鍵關閉此視窗…");
+            Console.ReadKey();
+        }
+        private static bool IsRunAsAdmin()
+        {
+            using var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        private static void CheckRegistryIcon(string path)
+        {
+            bool isRegistered = RegistryIcon.IsFileExtensionRegistered(ExtensionName);
+
+            if (isRegistered)
+            {
+                Console.WriteLine("." + ExtensionName + " 已經註冊。");
+            }
+            else
+            {
+                Console.WriteLine("." + ExtensionName + " 未註冊。");
+                if (File.Exists(path))
+                {
+                    RegistryIcon.SetFileExtensionIcon(ExtensionName, path);
+                }
+                else
+                {
+                    Console.WriteLine("檔案路徑不存在：" + path);
+                }
+            }
+        }
+    }
+}
---
+++ FileExtensionInstaller/FileExtensionInstaller/RegistryIcon.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace FileExtensionInstaller
+{
+    public class RegistryIcon
+    {
+        /// <summary>
+        /// 存放圖標的註冊資料夾名稱
+        /// </summary>
+        private const string DefaultIcon = "ExtensionIcon";
+        /// <summary>
+        /// 設置副檔名圖標
+        /// </summary>
+        public static void SetFileExtensionIcon(string extension, string iconPath)
+        {
+            try
+            {
+                // 創建或打開HKEY_CLASSES_ROOT\.extension
+                using (RegistryKey fileExtKey = Registry.ClassesRoot.CreateSubKey("." + extension))
+                {
+                    if (fileExtKey != null)
+                    {
+                        fileExtKey.SetValue(null, DefaultIcon);
+
+                        using (RegistryKey iconKey = Registry.ClassesRoot.CreateSubKey("." + extension + "\\" + DefaultIcon))
+                        {
+                            if (iconKey != null)
+                            {
+                                iconKey.SetValue(null, iconPath);
+                                Console.WriteLine($".{extension} 完成註冊。");
+                            }
+                            else
+                            {
+                                Console.WriteLine("無法創建或打開圖標註冊表項。");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("無法創建或打開文件擴展名註冊表項。");
+                    }
+                }
+
+                // 更新文件關聯
+                Process.Start("cmd.exe", $"/C assoc {extension}={DefaultIcon}");
+                Process.Start("cmd.exe", $"/C ftype {DefaultIcon}=\"{iconPath}\"");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("發生錯誤：" + ex.Message);
+            }
+        }
+        /// <summary>
+        /// 檢查副檔名是否已註冊
+        /// </summary>
+        public static bool IsFileExtensionRegistered(string extension)
+        {
+            try
+            {
+                using RegistryKey fileExtKey = Registry.ClassesRoot.OpenSubKey("." + extension);
+                return fileExtKey != null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("發生錯誤：" + ex.Message);
+                return false;
+            }
+        }
+    }
+}
